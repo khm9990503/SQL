61ccdb2666f5356c38ec42427661353876e5deab
khm9990503 <khm9990503@gmail.com>
2022-09-01 12:59:46 +0900

    db

--- 제3장 DB 생성.sql
+++ 제3장 DB 생성.sql
@@ -1,10 +1,11 @@
 #날짜 : 2022/08/24
 #이름 : 구홍모
 #내용 : 제3장 데이터베이스 관리
-
 #실습 3-1
-create database `testdb`;
-
+create database `java1_test_db`;
+create user 'java1_test'@'%' identified by '1234';
+grant all privileges on `java1_test_db`.* to 'java1_test'@'%';
+flush privileges;
 #실습 3-2
 #실습 3-3
 #실습 3-4
